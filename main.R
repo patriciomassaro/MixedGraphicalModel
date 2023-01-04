38d12f564bcfc4bf84a1ad929903a03eb5d0c77d
patricio.massaro <patomassaro@gmail.com>
2023-01-04 20:16:38 +0100

    Added penalization with soft thresholding

--- main.R
+++ main.R
@@ -17,8 +17,95 @@ force_simmetry <- function(input_matrix){
   
 }
 
+calculate_penalization <- function(matrix_beta,matrix_rho,matrix_phi,lambda,n,p,q,levels_per_variable,t)
+{
+  # Calculate the penalization term
+  # matrix_beta: the matrix of the beta parameters (p,p)
+  # matrix_rho: the matrix of the rho parameters (p,q)
+  # matrix_phi: the matrix of the phi parameters (q,q)
+  # lambda: the lambda parameter
+  # n: the number of records
+  # p: the number of continuous variables
+  # q: the number of discrete variables
+  # return: the penalization term
+
+  # Get the cumsum of the levels_per_variable
+  cumsum_levels <- cumsum(levels_per_variable)
+  
+  #Lambda is hardcoded
+  lambda <- 5 * sqrt((log(p+q))/n) * t
+
+  # Do the thresholding ( Proximal gradient)
+  beta_weights_aux <- matrix(1,p,p)
+  beta_weights <- matrix(pmax(0,beta_weights_aux - t/abs(matrix_beta)),p,p)
+  matrix_beta <- matrix_beta * beta_weights
+  
+  # Calculate the norm
+  norm_beta <- sum(abs(matrix_beta))
+
+  # Calculate the penalization term for rho matrix
+  norm_rho <- 0
+  # For all continuous
+  for (i in 1:p){
+  # For all discrete
+    for (j in 1:q){
+      levels_in_this_varible = levels_per_variable[j]
+      cummulated_levels_in_this_variable <- cumsum_levels[j]
+      lower_bound <- cummulated_levels_in_this_variable-levels_in_this_varible+1
+      upper_bound <- cummulated_levels_in_this_variable
+      aux_matrix_rho <- matrix_rho[i,lower_bound:upper_bound]
+      
+      # calculate weights
+      weights_aux_matrix_rho <- matrix(1,1,upper_bound - lower_bound + 1)
+      weights_aux_matrix_rho <- matrix(pmax(0,weights_aux_matrix_rho - t/abs(aux_matrix_rho)),
+                               1,
+                               upper_bound - lower_bound +1)
+      aux_matrix_rho <-aux_matrix_rho * weights_aux_matrix_rho
+      
+      
+      norm_rho <- norm_rho + norm(aux_matrix_rho, type="2")
+    }
+  }
+    
+    
+  # Calculate the penalization term for the phi matrix
+  norm_phi<-0
+
+    # For all discrete
+  for (j in 1:q){
+    levels_in_j = levels_per_variable[j]
+    cummulated_levels_in_j <- cumsum_levels[j]
+    #For all discrete greater than j and less than q
+    for (k in (j+1):q){
+      if (k<= q) {
+        levels_in_k = levels_per_variable[k]
+        cummulated_levels_in_k <- cumsum_levels[k]
+        lower_bound_j <- cummulated_levels_in_j-levels_in_j+1
+        lower_bound_k <- cummulated_levels_in_k-levels_in_k+1
+        upper_bound_j <- cummulated_levels_in_j
+        upper_bound_k <- cummulated_levels_in_k
+        
+        aux_matrix_phi <- matrix_phi[lower_bound_j:upper_bound_j,lower_bound_k:upper_bound_k]
+        # Apply weights
+        weights_aux_matrix_phi <- matrix(1,upper_bound_j - lower_bound_j +1 ,upper_bound_k - lower_bound_k+1)
+        weights_aux_matrix_phi <- matrix(pmax(0,weights_aux_matrix_phi - t/abs(aux_matrix_phi)),
+                                         upper_bound_j - lower_bound_j +1,
+                                         upper_bound_k - lower_bound_k +1) 
+        aux_matrix_phi <- aux_matrix_phi * weights_aux_matrix_phi
+        norm_phi <- norm_phi + norm(aux_matrix_phi, type="F")
+      }
+
+      
+    }
+  }
+    
+  penalization = lambda * (norm_phi+norm_rho+norm_beta)
+    
+  return(penalization)
+}
+
 create_simmmetryc_matrix<- function(nrows,ncols){
-  mat <- matrix(rnorm(ncols*nrows,mean=0,sd=0.001),
+  mat <- matrix(rnorm(ncols*nrows,mean=0,sd=0.1),
                 nrows,
                 ncols)
   
@@ -173,6 +260,8 @@ get_continuous_discrete <- function(data){
 
 #load the data
 data <- read.csv("data/Mid-Atlantic_Wage_Data_974_40.csv")
+# convert year column to str
+data[,3] <- as.character(data[,3])
 data <- clean_survey_data(data)
 data <- get_continuous_discrete(data)
 
@@ -184,7 +273,11 @@ levels_per_variable <- sapply(Y, function(x) length(levels(x)))
 total_levels <- sum(levels_per_variable)
 
 # Number of continouous variables
-total_continuous = NCOL(X)
+total_continuous <- NCOL(X)
+# Number of discrete variables
+total_discrete <- NCOL(Y)
+# Rows
+total_rows <- NROW(X)
 
 # Initiate parameters
 matrix_beta <- create_simmmetryc_matrix(total_continuous,total_continuous)
@@ -194,6 +287,16 @@ matrix_rho <- create_simmmetryc_matrix(total_continuous,total_levels)
 matrix_phi <- create_simmmetryc_matrix(total_levels,total_levels)
 
 l <- calculate_pseudo_likelihood(X,Y,matrix_beta,vector_alpha,matrix_rho,matrix_phi,levels_per_variable,total_levels)
+print(l)
+l <- l+ calculate_penalization(matrix_beta = matrix_beta,
+                               matrix_rho = matrix_rho,
+                               matrix_phi = matrix_phi,
+                               p = total_continuous,
+                               q = total_discrete,
+                               n = total_rows,
+                               levels_per_variable = levels_per_variable,
+                               t=1
+                               )
 
 print(l)
 
